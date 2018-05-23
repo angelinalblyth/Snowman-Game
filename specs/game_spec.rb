329fa397c74cdd7bf79baeb61e4b8df379c9cc45
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-23 16:01:36 +0100

    Added some tests to the hiddenword

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -17,4 +17,13 @@ class GameTest < MiniTest::Test
     assert_equal("HiddenWord", @game.hiddenword)
   end
 
+
+
+
+  # we need to take string "Hello", place each letter into an array. Enter guessed letter.Iterate over each letter now in the array with the guess letter. If it sees the letter print true. Iterate over the rest of the letter incase guessed letter is there more than once. If no letter is found, print false.
+
+  #Display hiddenword as * if the letter in the string has not been guessed. If guessed print out the word, with the guessed letter visible and the rest still as *.
+
+  #Each letter guessed, removed a life from chances. If chances = 0 then player has no more guesses. 
+
 end
